2ad35110535861dc20f7eb20b677e6fde5254704
AnnasMaulana <ashabulkaff@gmail.com>
2024-08-24 15:38:26 +0700

    commit-syntax

--- Main.py
+++ Main.py
@@ -4,3 +4,15 @@ print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
 print('Ibu menjawab, "Beli satu botol susu"')
 print('Maka budi berangkat ke toko')
 print("Dan mulai berbelanja")
+
+# Percabangan
+jumlah_botol_susu = 173
+
+if jumlah_botol_susu > 0:
+    print('Budi mengecek harga, dan ternyata uangnya cukup')
+    print('Budi membeli 1 botol susu')
+else:
+    print('Budi tidak jadi membeli 1 botol susu')
+
+print('Budi pulang ke rumah')
+print('Memberikan hasilnya kepada ibu')
