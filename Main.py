8ec7f95b8ca992df40467a6e4fd21329d1868fd5
AnnasMaulana <ashabulkaff@gmail.com>
2024-08-24 10:56:00 +0700

    commit-syntax

---
+++ Main.py
@@ -0,0 +1,6 @@
+# Sekuential
+print('Ibu berkata, "Pergi ke toko"')
+print('Budi menjawab, "Baik, apa yang harus saya lakukan di toko?"')
+print('Ibu menjawab, "Beli satu botol susu"')
+print('Maka budi berangkat ke toko')
+print("Dan mulai berbelanja")
