74dda147747076c28f7a7f941b90b239e51e31bd
AnnasMaulana <ashabulkaff@gmail.com>
2024-08-24 17:35:47 +0700

    commit-logic

--- Main.py
+++ Main.py
@@ -6,11 +6,18 @@ print('Maka budi berangkat ke toko')
 print("Dan mulai berbelanja")
 
 # Percabangan
-jumlah_botol_susu = 173
+jumlah_botol_susu = 75
+jumlah_telur = 1571
+print(f'Jumlah botol susu {jumlah_botol_susu} btl')
+print(f'Jumlah telur {jumlah_telur} butir')
 
 if jumlah_botol_susu > 0:
     print('Budi mengecek harga, dan ternyata uangnya cukup')
     print('Budi membeli 1 botol susu')
+    if jumlah_telur > 0:
+        print('Budi membeli 6 butir telur')
+    else:
+        print('Budi tidak jadi beli telur')
 else:
     print('Budi tidak jadi membeli 1 botol susu')
 
